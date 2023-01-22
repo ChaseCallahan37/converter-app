d2accfb01d6406d1c9ef259f63788eafac63b7ac
ChaseCallahan37 <ctcallahan2@crimson.ua.edu>
2023-01-22 16:30:41 -0600

    changed port to 5000 from 3000

--- api/src/app.js
+++ api/src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const port = 3000;
+const port = 5000;
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve("./src/views/root.html"));
